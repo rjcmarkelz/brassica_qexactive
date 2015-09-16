ad54063b629ac36384c719424adf189bae158f13
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-09-15 22:29:33 -0500

    example working

--- scripts/met_learn.R
+++ scripts/met_learn.R
@@ -9,5 +9,31 @@ list.files(path2file, recursive = TRUE)
 library(xcms)
 cdffiles <- list.files(path2file, recursive = TRUE, full.names = TRUE)
 xset <- xcmsSet(cdffiles)
-xset
+str(xset)
 plot(xset)
+
+?xcms
+
+xset <- group(xset)
+
+#retention time correction
+xset2 <- retcor(xset, family = "symmetric", plottype = "mdevden")
+xset2 <- group(xset2, bw = 10)
+xset3 <- fillPeaks(xset2)
+xset3
+
+reporttab <- diffreport(xset3, "WT", "KO", "example", 10, metlin = 0.15, h = 480, w = 640)
+str(reporttab)
+reporttab[1:4, ]
+
+gt <- groups(xset3)
+colnames(gt)
+groupidx1 <- which(gt[,"rtmed"] > 2600 & gt[,"rtmed"] < 2700 & gt[,"npeaks"] == 12)
+groupidx2 <- which(gt[,"rtmed"] > 3600 & gt[,"rtmed"] < 3700 & gt[,"npeaks"] == 12)
+eiccor <- getEIC(xset3, groupidx = c(groupidx1, groupidx2))
+eicraw <- getEIC(xset3, groupidx = c(groupidx1, groupidx2), rt = "raw")
+
+plot(eicraw, xset3, groupidx = 1)
+plot(eicraw, xset3, groupidx = 2)
+plot(eiccor, xset3, groupidx = 1)
+plot(eiccor, xset3, groupidx = 2)

324a9475bf60382d6baa26b3f021d7cd45df86e2
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-10-06 11:23:09 -0700

    lab meeting absorbance plot

--- scripts/brassica_xcms_full.R
+++ scripts/brassica_xcms_full.R
@@ -32,3 +32,40 @@ eiccor <- getEIC(br_set2, groupidx = c(groupidx1, groupidx2))
 eiccor
 
 plot(eiccor, br_set2)
+
+
+setwd("/Users/Cody_2/git.repos/brassica_qexactive/data/absorbance")
+UV_641 <- read.table("641abs_clean.csv", sep = ",", header = TRUE)
+UV_611 <- read.table("611abs_clean.csv", sep = ",", header = TRUE)
+head(UV_641)
+
+plot(UV_641)
+plot(UV_611)
+
+UV_merged <- merge(UV_641, UV_611, by = "Wavelength")
+UV_merged
+
+library(ggplot2)
+library(reshape2)
+colnames(UV_merged)[2:3] <- paste(c("Quercetin", "Kaempferol"))
+UV_melt <- melt(UV_merged, id.vars = c("Wavelength"))
+head(UV_melt)
+
+
+abs_plot <- ggplot(UV_melt) + 
+  geom_line(aes(x = Wavelength, y = value, color = variable), size = 1) +
+  xlab("Wavelength") + ylab("Absorbance") +
+  # facet_grid(variable ~ .) +
+  # scale_x_continuous(limits = c(400, 450)) 
+  theme(axis.title.x = element_text(face="bold", size=20),
+           axis.text.x  = element_text(face="bold",size=14),
+           axis.title.y = element_text(face="bold", size=20),
+           axis.text.y  = element_text(face="bold",size=14),
+           panel.grid.major = element_blank(),
+           panel.grid.minor = element_blank(),
+           panel.background = element_blank(),
+           panel.border = element_rect(fill = NA, color = "black"),
+           strip.text.y = element_text(size = 12, face = "bold"))
+abs_plot
+
+

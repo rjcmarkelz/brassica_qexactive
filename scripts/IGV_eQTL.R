5997b3adf45dc84dde032beb6eef577db7c57dd8
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-10-15 20:20:40 -0700

    integrative genome viewer for sequence comparison

---
+++ scripts/IGV_eQTL.R
@@ -0,0 +1,10 @@
+#infile genomic coordinates of genes
+setwd("/Users/Cody_2/git.repos/brassica_genome_db/raw_data")
+transcripts <- read.table("transcripts_eqtl_start_stop_eqtl.csv", sep = ",", header = TRUE)
+head(transcripts)
+str(transcripts)
+dim(transcripts)
+
+# open genomic reference sequence in IGV
+# open all BAM files to check for differences in mapped reads
+# manipulate files as below
